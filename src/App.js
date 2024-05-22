6d675413187637860a6b9a7afe3b592347d54b83
sasukemiyazawa <b1020251@fun.ac.jp>
2024-05-22 11:37:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
 function App() {
+  const base_url = "http://localhost:8000"
+  // const filePass = "Documents/2024前期/プロ通/pulp_practice2.ipynb"
+  // const token = "token 80b59460699e50616d082f1b938061d1cf33f3d6748554f8"
+  const getNotebook = () => {
+    axios.get(base_url)
+      .then((res) => {
+        console.log(res)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={getNotebook}>push</button>
     </div>
   );
 }
